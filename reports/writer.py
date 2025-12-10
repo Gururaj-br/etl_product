63a1c6df55e8a3b0b513f25194079012017dc055
guru <gururaj.br@dataweave.com>
2025-12-10 22:27:30 +0530

    updated code and moved file and folder details to init method

--- reports/writer.py
+++ reports/writer.py
@@ -2,22 +2,21 @@ import pandas as pd
 import os
 from config import *
 
-report_folder = Report_Config.REPORT_FOLDER_PATH
-
-if not os.path.exists(report_folder):
-    os.makedirs(report_folder)
-
-output_file = os.path.join(
-    report_folder, 
-    Report_Config.REPORT_FILE_NAME.format(pd.Timestamp.now().strftime("%Y%m%d_%H:%M%S"))
-)
-
 class ReportWriter:
     def __init__(self, data):
         self.data = data
+        self.report_folder = Report_Config.REPORT_FOLDER_PATH
+
+        if not os.path.exists(self.report_folder):
+            os.makedirs(self.report_folder)
+
+        self.output_file = os.path.join(
+            self.report_folder, 
+            Report_Config.REPORT_FILE_NAME.format(pd.Timestamp.now().strftime("%Y%m%d_%H:%M%S"))
+        )
 
     def write_report(self):
         df = pd.DataFrame(self.data)
         df.rename(columns=Report_Config.HEADERS, inplace=True)
-        df.to_csv(output_file, index=False)
-        print(f"Report written to {output_file}")
+        df.to_csv(self.output_file, index=False)
+        print(f"Report written to {self.output_file}")
